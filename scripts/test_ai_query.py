b440440a890c6e43597a87f9abcf1a462b6916d9
RaghavShahMU <109519663+RaghavShahMU@users.noreply.github.com>
2026-03-07 04:46:32 +0530

    Update test_ai_query.py

--- scripts/test_ai_query.py
+++ scripts/test_ai_query.py
@@ -1,7 +1,7 @@
 from analytics.ai_query_engine import ask_question
 
 
-question = "Top 10 wicket takers in IPL history"
+question = "When Bumrah bowls 19th Over of the 2nd innings, what is the probabilitiy that his team wins? Could you also bring stats around it? Like when how many times it worked or didn't worked. Also a basic distribution of the opponent team's score"
 
 sql, result = ask_question(question)
 
